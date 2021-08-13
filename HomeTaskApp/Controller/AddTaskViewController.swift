80ae01f66ccd60ed153bd80a48a4fed36119d136
Nao-RandD <nao.r.and.d@gmail.com>
2021-08-14 00:36:16 +0900

    タスクの追加画面を追加

---
+++ HomeTaskApp/Controller/AddTaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTaskViewController.swift
+//  HomeTaskApp
+//
+//  Created by Naoyuki Kan on 2021/08/13.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeModal() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
